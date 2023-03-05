01f60c778f9c0cafbb462e3d5d1f933019254f3a
Philipe Ayres <p.ayres9@gmail.com>
2023-03-05 14:27:13 -0500

    google button

--- src/Components/Sidebarcontent/Sidebar.jsx
+++ src/Components/Sidebarcontent/Sidebar.jsx
@@ -6,6 +6,7 @@ import {AiOutlineStock} from 'react-icons/ai'
 import {ImStatsBars} from 'react-icons/im';
 import {BsDoorOpenFill, BsDoorOpen} from 'react-icons/bs';
 import { UserAuth } from '../../FirebaseAuth/AuthContext';
+import GoogleButton from 'react-google-button';
 
 const Sidebar = () => {
   const { googleSignIn, user } = UserAuth();
@@ -106,12 +107,13 @@ const Sidebar = () => {
             Logout
         </h3>
         :
-        <h3 className='text' onClick={handleGoogleSignIn}>
-          <svg style={{width:'60px', height:'38px',alignItems:'center', justifyContent:'center', display:'inline-flex', flexDirection:'row'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-          </svg>
-            Sign in
-        </h3>
+        <GoogleButton style={{marginTop:'20px', color:'black', borderRadius:'4px'}} type="light"  onClick={handleGoogleSignIn}/>
+        // <h3 className='text' onClick={handleGoogleSignIn}>
+        //   <svg style={{width:'60px', height:'38px',alignItems:'center', justifyContent:'center', display:'inline-flex', flexDirection:'row'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        //     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+        //   </svg>
+        //     Sign in
+        // </h3>
         }
         
         {/* {user?.displayName? <p style={{fontSize:'20px'}}>Hi, {user?.displayName}!</p> : null} */}
